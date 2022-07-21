f74a721e355ebebe115b1d57969c6618827859c0
DHANUSHREE-K-T <dhanushreekt22@gmail.com>
2022-07-21 16:11:10 +0530

    try pushing

--- mf-next/apps/card/src/app/app.tsx
+++ mf-next/apps/card/src/app/app.tsx
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
-  // Your style here
+  color:red;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
 `;
 
 export function App() {
   return (
     <StyledApp>
       <h1>Card</h1>
+
     </StyledApp>
   );
 }
